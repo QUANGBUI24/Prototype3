d8beca95d6405d9dddc4274df024a4a50eaebad4
Quang Bui <qhbui@millersville.edu>
2024-09-19 17:37:09 -0400

    Finish saving and loading

--- UML_MANAGER/uml_manager.py
+++ UML_MANAGER/uml_manager.py
@@ -17,85 +17,11 @@ List of last date modified:
 import json
 import os
 
-import UML_UTILITY.SAVE_LOAD.save_load as SAVE_LOAD
 
 ################################################################
-# LOADING SAVED FILE'S NAMES FROM JSON FILE TO GLOBAL LIST #
-saved_file_name_list = SAVE_LOAD.load_name()
-# LOADING DATA FROM JSON FILE TO GLOBAL DICTIONARY #
-data_list = [[], []]
-# Create a class so that we can display it or sort it alphabetically easily
-class_list: list[str] = []
-# Get list of classes and its attributes
-class_and_attr_list = data_list[0]
-# Get list of relationships
-relationship_list = data_list[1]
-# Add class name to class_list
-for dictionary in class_and_attr_list:
-    class_list.append(dictionary["class_name"])
-
-
-def update_data(new_data_list: list):
-    global data_list, class_list, class_and_attr_list, relationship_list, class_list
-    data_list = new_data_list
-    class_and_attr_list = data_list[0]
-    relationship_list = data_list[1]
-    class_list.clear()
-    for dictionary in class_and_attr_list:
-        class_list.append(dictionary["class_name"])
-
-
-# Save data to the JSON file
-def save_data_to_json(data: dict[str, list[str]], file_name: str):
-    file_path = f"UML_UTILITY/SAVE_LOAD/SAVED_FILES/{file_name}.json"
-    try:
-        if file_name in saved_file_name_list:
-            with open(file_path, "r") as file:
-                data = json.load(file)
-                return
-        with open(file_path, "w") as file:
-            json.dump(data, file, indent=4)
-            print("Successfully save data!")
-    except FileNotFoundError:
-        print(f"File {file_path} not found.")
-        return None
-    except json.JSONDecodeError:
-        print(f"Error decoding JSON from {file_path}.")
-        return None
-
-
-# Load data from the JSON file
-def load_data_from_json(file_name: str):
-    file_path = f"UML_UTILITY/SAVE_LOAD/SAVED_FILES/{file_name}.json"
-    try:
-        with open(file_path, "r") as file:
-            data = json.load(file)
-            return data
-    except FileNotFoundError:
-        print(f"File {file_path} not found.")
-        return None
-    except json.JSONDecodeError:
-        print(f"Error decoding JSON from {file_path}.")
-        return None
-
-
-# Saving saved file's names to JSON file
-def save_name_list(name_list: list[str]):
-    file_path = "UML_UTILITY/SAVE_LOAD/SAVED_FILES/NAME_LIST.json"
-    try:
-        with open(file_path, "w") as file:
-            json.dump(name_list, file, indent=4)
-    except FileNotFoundError:
-        print(f"File {file_path} not found.")
-        return None
-    except json.JSONDecodeError:
-        print(f"Error decoding JSON from {file_path}.")
-        return None
-
-
 # Load all saved file's name from the JSON file "NAME_LIST.json"
 def load_name():
-    file_path = "UML_UTILITY/SAVE_LOAD/SAVED_FILES/NAME_LIST.json"
+    file_path = "UML_UTILITY/SAVED_FILES/NAME_LIST.json"
     try:
         # Check if file is empty
         if os.stat(file_path).st_size == 0:
@@ -110,3 +36,83 @@ def load_name():
     except json.JSONDecodeError:
         print(f"Error decoding JSON from {file_path}.")
         return None
+
+
+def update_data(new_data_list: list):
+    global data_list, class_list, class_and_attr_list, relationship_list, class_list
+    data_list = new_data_list
+    class_and_attr_list = data_list[0]
+    relationship_list = data_list[1]
+    class_list.clear()
+    for dictionary in class_and_attr_list:
+        class_list.append(dictionary["class_name"])
+
+
+# Save data to the JSON file
+def save_data_to_json(file_name: str):
+    file_path = f"UML_UTILITY/SAVED_FILES/{file_name}.json"
+    try:
+        # Check if the file exists
+        if not os.path.exists(file_path):
+            # If the file doesn't exist, create it and write the data
+            with open(file_path, "w") as json_file:
+                json.dump(data_list, json_file, indent=4)
+                return
+        # If the file exists, load the current data, update it, and save it
+        if file_name in saved_file_name_list:
+            # Open file to update
+            with open(file_path, "r") as file:
+                data = json.load(file)
+                data = data_list
+            # Open file to save data
+            with open(file_path, "w") as file:
+                json.dump(data, file, indent=4)
+            print("Succesfully save data!")
+
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from {file_path}.")
+        return None
+
+
+# Load data from the JSON file
+def load_data_from_json(file_name: str):
+    file_path = f"UML_UTILITY/SAVED_FILES/{file_name}.json"
+    try:
+        with open(file_path, "r") as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from {file_path}.")
+        return None
+
+
+# Saving saved file's names to JSON file
+def save_name_list(name_list: list[str]):
+    file_path = "UML_UTILITY/SAVED_FILES/NAME_LIST.json"
+    try:
+        with open(file_path, "w") as file:
+            json.dump(name_list, file, indent=4)
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from {file_path}.")
+        return None
+
+
+# LOADING SAVED FILE'S NAMES FROM JSON FILE TO GLOBAL LIST #
+saved_file_name_list = load_name()
+# LOADING DATA FROM JSON FILE TO GLOBAL DICTIONARY #
+data_list = [[], []]
+# Create a class so that we can display it or sort it alphabetically easily
+class_list: list[str] = []
+# Get list of classes and its attributes
+class_and_attr_list = data_list[0]
+# Get list of relationships
+relationship_list = data_list[1]
+# Add class name to class_list
+for dictionary in class_and_attr_list:
+    class_list.append(dictionary["class_name"])
